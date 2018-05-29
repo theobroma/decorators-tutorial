45afc0265e89b75de29ddb9603066b27b36b79fa
Aleksandr Siryi <theobroma222@gmail.com>
2018-05-29 21:46:30 +0300

    rxjs

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import Rx from 'rxjs';
+
+const button = document.querySelector('.button');
+const label = document.querySelector('h4');
+
+const clickStream = Rx.Observable.fromEvent(button, 'click');
+clickStream.subscribe(x => console.log(x));
+
+// const doubleClickStream;
+
+// doubleClickStream.subscribe(event => {
+//   label.textContent = 'double click';
+// });
+
+// doubleClickStream.throttle(1000).subscribe(suggestion =>{
+//   label.textContent='-';
+// });
